97989c2e15fb282a0d39551937046d6daa8e1720
Alexander Shchirov <shchirov1998@mail.ru>
2024-07-31 19:29:58 +0300

    secondTask

---
+++ src/app/app-reducer.ts
@@ -0,0 +1,27 @@
+export type RequestStatusType = 'idle' | 'loading' | 'succeeded' | 'failed'
+
+const initialState = {
+  status: 'idle' as RequestStatusType,
+  error: null as string | null,
+}
+
+type AppStateType = typeof initialState
+
+export const appReducer = (state: AppStateType = initialState, action: ActionsType): AppStateType => {
+  switch (action.type) {
+    case 'APP/SET-STATUS':
+      return{
+        ...state,
+        status: action.status
+      }
+    default:
+      return state
+  }
+}
+
+type ActionsType = ReturnType<typeof setAppStatus>
+
+export const setAppStatus = (status: RequestStatusType) => ({
+  type: 'APP/SET-STATUS' as const,
+  status
+})
